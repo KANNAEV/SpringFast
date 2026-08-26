b528966023e3276d6939d08c1a0a95eb4095e5d0
GADZHI <kannaev@gmail.com>
2026-08-26 03:20:08 +0300

    ok

---
+++ Chapter2/task5/src/main/java/com/example/model/Product.java
@@ -0,0 +1,22 @@
+package com.example.task1.model;
+
+public class Product {
+	private String name;
+	private Long price;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+}
